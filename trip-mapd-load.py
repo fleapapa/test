9f252857a1e368f3b0f187dcf6ba255168e6959e
Flea Papa <fleapapa@gmail.com>
2017-09-28 18:01:46 -0700

    upd

--- trip-mapd-load.py
+++ trip-mapd-load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys, getopt
 import csv
 import threading
 import time
@@ -11,9 +11,36 @@ import pymapd
 from pymapd import connect
 
 # change these per your environment
-NWRITER = 1								# number of writer (LOAD   query) threads
-NREADER = 1								# number of reader (SELECT query) threads
+# could make csv file path and table name also configurable but not a priority for now:) 
 CSVPATH = '/mnt/trip/csvfiles/*.csv'	# NY taxcab trip CSV file paths
+re_create_table = False					# -c to remove and create table before begin test
+nwriter = 0								# -w number of writer (LOAD   query) threads
+nreader = 1								# -r number of reader (SELECT query) threads
+
+def usage():
+	print sys.argv[0], ' cmd args error :)'
+	print "usage: %s [-h][-c][-w nw][-r nr]" % os.path.basename(sys.argv[0])
+	print "\t -h          show this help"
+	print "\t -c          delete and re-create table"
+	print "\t -r nr       fork nr readers"
+	print "\t -w nw       fork nw writers"
+	sys.exit(-1)
+	
+# parse args
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "w:r::ch")
+except getopt.GetoptError:
+	usage()
+
+for opt, arg in opts:
+	if opt == '-c':
+		re_create_table = True
+	elif opt == '-h':
+		usage()
+	elif opt == '-r':
+		nreader = int(arg)
+	elif opt == '-w':
+		nwriter = int(arg)
 
 # this signals end of data loading
 end_of_writes = False
@@ -62,62 +89,66 @@ def reader(ith):
 		
 	con.close()
 
-# drop table trip1 if exists
-con = connect(user=os.environ['MAPD_USERNAME'], password=os.environ['MAPD_PASSWORD'], host="localhost", dbname="mapd")
-cur = con.cursor()
-
-try:
-	print "drop table trip1 if exists..."
-	cur.execute("drop table trip1;")
-except pymapd.exceptions.Error as e:
-	#print e
-	pass
-
-print "create table trip1..."
-cur.execute(
-'''CREATE TABLE trip1 (
-    medallion               TEXT ENCODING DICT,
-    hack_license            TEXT ENCODING DICT,
-    vendor_id               TEXT ENCODING DICT,
-    rate_code_id            SMALLINT,
-    store_and_fwd_flag      TEXT ENCODING DICT,
-    pickup_datetime         TIMESTAMP,
-    dropoff_datetime        TIMESTAMP,
-    passenger_count         SMALLINT,
-    trip_time_in_secs       INTEGER,
-    trip_distance           DECIMAL(14,2),
-    pickup_longitude        DECIMAL(14,2),
-    pickup_latitude         DECIMAL(14,2),
-    dropoff_longitude       DECIMAL(14,2),
-    dropoff_latitude        DECIMAL(14,2)
-    ) WITH (FRAGMENT_SIZE=75000000);
-'''
-)
-cur.close()
-con.close()
+if re_create_table:
+	# drop table trip1 if exists
+	gcon = connect(user=os.environ['MAPD_USERNAME'], password=os.environ['MAPD_PASSWORD'], host="localhost", dbname="mapd")
+	gcur = gcon.cursor()
+	
+	try:
+		print "drop table trip1 if exists..."
+		gcur.execute("drop table trip1;")
+	except pymapd.exceptions.Error as e:
+		#print e
+		pass
+	
+	print "create table trip1..."
+	gcur.execute(
+	'''CREATE TABLE trip1 (
+		medallion               TEXT ENCODING DICT,
+		hack_license            TEXT ENCODING DICT,
+		vendor_id               TEXT ENCODING DICT,
+		rate_code_id            SMALLINT,
+		store_and_fwd_flag      TEXT ENCODING DICT,
+		pickup_datetime         TIMESTAMP,
+		dropoff_datetime        TIMESTAMP,
+		passenger_count         SMALLINT,
+		trip_time_in_secs       INTEGER,
+		trip_distance           DECIMAL(14,2),
+		pickup_longitude        DECIMAL(14,2),
+		pickup_latitude         DECIMAL(14,2),
+		dropoff_longitude       DECIMAL(14,2),
+		dropoff_latitude        DECIMAL(14,2)
+		) WITH (FRAGMENT_SIZE=75000000);
+	'''
+	)
+	gcur.close()
+	gcon.close()
 
 # one thread for each csv to simulate concurrent writes and reads
 nth = len(csv_files)
 wthreads = []
 rthreads = []
 
-# fork some reader threads to test concurrency
-print "launch %d reader threads..." % NREADER
-for i in range(0, NREADER):
-	t = threading.Thread(target=reader, args=[i])
-	rthreads.append(t)
-	t.start()
-
 # fork nth writer threads
-print "launch %d writer threads..." % NWRITER
-for i in range(0, NWRITER):
+print "launch %d writer threads..." % nwriter
+for i in range(0, nwriter):
 	t = threading.Thread(target=writer, args=[i])
 	wthreads.append(t)
+	t.start()
+
+# fork some reader threads to test concurrency
+print "launch %d reader threads..." % nreader
+for i in range(0, nreader):
+	t = threading.Thread(target=reader, args=[i])
+	rthreads.append(t)
 	t.start()
 
 # wait for writers to finish
 for t in wthreads: t.join()
 
+# when nwriter==0, if we set end_of_writes to True immediately, no reader will
+# has any chance to send SELECT query to mapd, so sleep a while for SELECT to be sent.
+time.sleep(2)
 end_of_writes = True
 
 # wait for readers to finish
